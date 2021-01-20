b2310918c0bdc0d80ee84eca5296c9229302e71e
cychann <skcbdlhc129@naver.com>
2021-01-21 00:04:00 +0900

    Start + Timer + Stop

--- Browser/carrot-game/main2.js
+++ Browser/carrot-game/main2.js
@@ -1,13 +1,95 @@
 'use strict'
 
 const CARRIT_SIZE = 80;
+const CARROT_COUNT = 5;
+const BUG_COUNT = 5;
+const GAME__DURATION_SEC = 5;
+
 const field = document.querySelector('.game__field');
 const fieldRect = field.getBoundingClientRect();
+const gameBtn = document.querySelector('.game__button');
+const gameTimer = document.querySelector('.game__timer');
+const gameScore = document.querySelector('.game__score');
+
+const popUp = document.querySelector('.pop-up');
+const popUpText = document.querySelector('.pop-up__message');
+const popUpRrefresh = document.querySelector('.pop-up__refresh');
+
+let started = false;
+let score = 0;
+let timer = undefined;
+
+gameBtn.addEventListener('click', () => {
+    if(started) {
+        stopGame();
+    }
+    else{
+        startGame();
+    }
+    started = !started;
+})
+
+function startGame() {
+    initGame();
+    showStopButton();
+    showTimerAndScroe();
+    startGameTimer();
+}
+
+function stopGame() {
+    stopGameTimer();
+    hideGameButton();
+    showPopUpWithText('REPLAY?');
+}
+
+function hideGameButton() {
+    gameBtn.style.visibility = 'hidden';
+}
+
+function showStopButton() {
+    const icon = gameBtn.querySelector('.fa-play');
+    icon.classList.add('fa-stop');
+    icon.classList.remove('fa-play');
+}
+
+function showTimerAndScroe() {
+    gameTimer.style.visibility = 'visible';
+    gameScore.style.visibility = 'visible';
+}
+
+function startGameTimer() {
+    let remainingTimeSec = GAME__DURATION_SEC;
+    updataTimerText(remainingTimeSec);
+    timer = setInterval(() => {
+        if(remainingTimeSec<=0) {
+            clearInterval(timer);
+            return;
+        }
+        updataTimerText(--remainingTimeSec);
+    }, 1000);
+}
+
+function stopGameTimer() {
+    clearInterval(timer);
+}
+
+function updataTimerText(time) {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    gameTimer.innerText = `${minutes}:${seconds}`;
+}
+
+function showPopUpWithText(text) {
+    popUpText.innerText = text;
+    popUp.classList.remove('pop-up--hide');
+}
+
 
 function initGame() {
-    console.log(fieldRect)
-    addItem('carrot', 5, 'carrot/img/carrot.png');
-    addItem('bug', 5, 'carrot/img/bug.png');
+    field.innerHTML = '';
+    gameScore.innerText = CARROT_COUNT;
+    addItem('carrot', CARROT_COUNT, 'carrot/img/carrot.png');
+    addItem('bug', BUG_COUNT, 'carrot/img/bug.png');
 }
 
 function addItem(className, count, imgPath) {
@@ -31,5 +113,3 @@ function addItem(className, count, imgPath) {
 function randomNumber(min, max) {
     return Math.random() * (max-min) + min;
 }
-
-initGame();
