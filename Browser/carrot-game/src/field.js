73ea29859071608af2508c756bb305a2f2540402
cychann <skcbdlhc129@naver.com>
2021-01-23 11:25:55 +0900

    Refactoring: field

---
+++ Browser/carrot-game/src/field.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const carrotSound = new Audio('carrot/sound/carrot_pull.mp3');
+const CARRIT_SIZE = 80;
+
+export default class Field {
+    constructor(carrotCount, bugCount) {
+        this.carrotCount = carrotCount;
+        this.bugCount = bugCount;
+        this.field = document.querySelector('.game__field');
+        this.fieldRect = this.field.getBoundingClientRect();
+        this.field.addEventListener('click', this.onClick);
+    }
+
+    init() {
+        this.field.innerHTML = '';
+        this.addItem('carrot', this.carrotCount, 'carrot/img/carrot.png');
+        this.addItem('bug', this.bugCount, 'carrot/img/bug.png');
+    }
+
+    setClickListener(onItemClick) {
+        this.onItemClick = onItemClick;
+    }
+
+    addItem(className, count, imgPath) {
+        const x1 = 0;
+        const y1 = 0;
+        const x2 = this.fieldRect.width - CARRIT_SIZE;
+        const y2 = this.fieldRect.height - CARRIT_SIZE;
+        for(let i=0; i<count; i++) {
+            const item = document.createElement('img');
+            item.setAttribute('class', className);
+            item.setAttribute('src', imgPath);
+            item.style.position = 'absolute';
+            const x = randomNumber(x1, x2);
+            const y = randomNumber(y1, y2);
+            item.style.left = `${x}px`;
+            item.style.top = `${y}px`;
+            this.field.appendChild(item);
+        }
+    }
+
+    onClick(event) {
+        const target = event.target;
+        if(target.matches('.carrot')) {
+            target.remove();
+            playSound(carrotSound);
+            this.onItemClick && this.onItemClick('carrot');
+        }
+        else if(target.matches('.bug')) {
+            this.onItemClick && this.onItemClick('bug');
+        }
+    };
+}
+
+function randomNumber(min, max) {
+    return Math.random() * (max-min) + min;
+}
+
+function playSound(sound) {
+    sound.currentTime = 0;
+    sound.play();
+}
