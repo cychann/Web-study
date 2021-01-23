c15f18a6919c7e91e75586163ad408f724776648
cychann <skcbdlhc129@naver.com>
2021-01-23 11:47:45 +0900

    Refactoring: Sound

--- Browser/carrot-game/src/field.js
+++ Browser/carrot-game/src/field.js
@@ -1,6 +1,6 @@
 'use strict';
+import * as sound from './sound.js';
 
-const carrotSound = new Audio('carrot/sound/carrot_pull.mp3');
 const CARRIT_SIZE = 80;
 
 export default class Field {
@@ -40,11 +40,11 @@ export default class Field {
         }
     }
 
-    onClick(event) {
+    onClick = event => {
         const target = event.target;
         if(target.matches('.carrot')) {
             target.remove();
-            playSound(carrotSound);
+            sound.playCarrot();
             this.onItemClick && this.onItemClick('carrot');
         }
         else if(target.matches('.bug')) {
@@ -55,9 +55,4 @@ export default class Field {
 
 function randomNumber(min, max) {
     return Math.random() * (max-min) + min;
-}
-
-function playSound(sound) {
-    sound.currentTime = 0;
-    sound.play();
 }
